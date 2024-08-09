99e5ede151e1da29b720e373fe790730deef9ada
DannyLien <danny.lien1109@gmail.com>
2024-08-09 21:09:06 +0800

    3-4, 元件, 輸入, 按鈕, logd;

--- app/src/main/java/com/tom/guess/MainActivity.kt
+++ app/src/main/java/com/tom/guess/MainActivity.kt
@@ -2,13 +2,36 @@ package com.tom.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import com.tom.guess.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String? = MainActivity::class.java.name
+    private lateinit var binding: ActivityMainBinding
     val secretNumber = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
 
     }//onCreate
+
+
+    fun check(view: View) {
+        val n = binding.number.text.toString().toInt()
+        println("number : $n")
+        Log.d(TAG, "number : ${n}")
+    }
+
 }//AppCompatActivity
+
+
+
+
+
+
